01fa302304ec534a50e0b49cbd76c9c140a869e4
sridharamesh <150352934+sridharamesh@users.noreply.github.com>
2023-11-16 15:20:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-
 df = pd.read_pickle('data_with_probability1.pkl')
 
 def get_top_n_category_recommendations(user_id, categories, N=5):
@@ -12,7 +11,7 @@ def get_top_n_category_recommendations(user_id, categories, N=5):
 
 st.title("RELATED PRODUCTS")
 st.image('amazon.jpg')
-    
+
 col1, col2, col3 = st.columns([1, 1, 1])
 with col2:
     st.sidebar.header('Set id:222643')
@@ -25,8 +24,10 @@ with col2:
         submit_button = st.form_submit_button("Get related products")
 
 if submit_button:
-    recommendations = get_top_n_category_recommendations(user_id, categories, N=5)
-    
+    user_data = df[(df['user_id'] == user_id) | (df['Category'].isin(categories))]
+    user_data = user_data.sort_values(by='reorder_probability', ascending=False)
+    recommendations = user_data.head(5)
+
     st.subheader(f"Top Products for User {user_id} in Selected Categories:")
     # Style the recommendation boxes
     box_style = """
@@ -39,20 +40,19 @@ if submit_button:
     color: black;
       """
 
-
-
-
     # Display each recommendation in a styled box
-    for i, row in recommendations.iterrows():
-        st.markdown(
-            f"""<div style="{box_style}">
-                <p><strong>Product Title:</strong> {row['Product Title']}</p>
-                <p><strong>Category:</strong> {row['Category']}</p>
-                <p><strong>Product ID:</strong> {row['Product Id']}</p>
-            </div>""",
-            unsafe_allow_html=True
-        )
-
+    if not recommendations.empty:
+        for i, row in recommendations.iterrows():
+            st.markdown(
+                f"""<div style="{box_style}">
+                    <p><strong>Product Title:</strong> {row['Product Title']}</p>
+                    <p><strong>Category:</strong> {row['Category']}</p>
+                    <p><strong>Product ID:</strong> {row['Product Id']}</p>
+                </div>""",
+                unsafe_allow_html=True
+            )
+    else:
+        st.warning("No data found for the given user ID and categories.")
 
     # Add a back button to reset the form
     if st.button("Go Back"):
